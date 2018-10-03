b2d5dc4328c2ed290e049739115bd0662dacc18c
Scott Thurston <scott.thurston@gmail.com>
2018-10-02 21:11:16 -0600

    Implement HTTP server and password request handler

--- hasher.go
+++ hasher.go
@@ -3,28 +3,79 @@ package main
 import (
 	"crypto/sha512"
 	"encoding/base64"
+	"flag"
 	"fmt"
-	hash2 "hash"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
 )
 
-func hash(password string) hash2.Hash {
-	mySha512 := sha512.New()
-	mySha512.Write([]byte(password))
-	return mySha512
+func encode(password string) string {
+	pwSha512 := sha512.New()
+	pwSha512.Write([]byte(password))
+	encodedPassword := base64.StdEncoding.EncodeToString(pwSha512.Sum(nil))
+	return encodedPassword
 }
 
-func encode(hashData hash2.Hash) string {
-	encHash := base64.StdEncoding.EncodeToString(hashData.Sum(nil))
-	return encHash
+func isPortValid(port string) bool {
+	const minPort = 1024
+	const maxPort = 9000
+
+	intPort, _ := strconv.Atoi(port)
+	var portIsValid = true
+
+	if intPort < minPort || intPort > maxPort {
+		portIsValid = false
+	}
+
+	return portIsValid
+}
+
+func getRandomInt() int {
+	const minimum = 5000
+	const maximum = 6000
+
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(maximum-minimum) + minimum
+}
+
+func getSleepInterval() time.Duration {
+	const nanosecondsPerMillisecond = 1000000
+
+	randSleep := getRandomInt()
+	randSleep = randSleep * nanosecondsPerMillisecond
+	return time.Duration(randSleep)
+}
+
+func passwordHandler(writer http.ResponseWriter, request *http.Request) {
+	time.Sleep(getSleepInterval())
+
+	if request.Method != http.MethodPost {
+		writer.WriteHeader(http.StatusBadRequest)
+		writer.Write([]byte("400 - Request method must be 'POST'"))
+	} else {
+		clearPassword := request.FormValue("password")
+		encodedPassword := encode(clearPassword)
+		encodedPassword = "\"" + encodedPassword + "\"\n"
+		writer.Write([]byte(encodedPassword))
+	}
 }
 
 func main() {
 	fmt.Println("Hasher")
+	const defaultPort string = "8080"
 
-	password := "angryMonkey"
+	portPtr := flag.String("port", defaultPort, "Listen port")
+	flag.Parse()
 
-	hashedPw := hash(password)
-	encPw := encode(hashedPw)
+	if !isPortValid(*portPtr) {
+		fmt.Printf("Port %s is invalid; setting to default (%s)\n", *portPtr, defaultPort)
+		*portPtr = defaultPort
+	}
 
-	fmt.Printf("base64 (enc):\t%s\n", encPw)
+	*portPtr = ":" + *portPtr
+
+	http.HandleFunc("/hash", passwordHandler)
+	http.ListenAndServe(*portPtr, nil)
 }
--- hasher_test.go
+++ hasher_test.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha512"
+	"net/http"
+	"net/url"
+	"strings"
 	"testing"
 )
 
@@ -10,22 +11,48 @@ func TestEnc(t *testing.T) {
 	testPw := "angryMonkey"
 	var expEncPw = "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="
 
-	testHash := hash(testPw)
-	testEncPw := encode(testHash)
+	testEncPw := encode(testPw)
 
 	if testEncPw != expEncPw {
 		t.Errorf("Calculated Base64 encoding did not match expected;\nExpected: '%s'\nReceived: '%s'\n", expEncPw, testEncPw)
 	}
 }
 
-func TestHash(t *testing.T) {
-	testPw := "angryMonkey"
-	expSha512 := sha512.New()
-	expSha512.Write([]byte(testPw))
-
-	testSha512 := hash(testPw)
-
-	if bytes.Compare(expSha512.Sum(nil), testSha512.Sum(nil)) != 0 {
-		t.Errorf("Calculated SHA512 hash did not match expected")
+func TestInvalidPort(t *testing.T) {
+	portTooLow := "1023"
+	if isPortValid(portTooLow) {
+		t.Errorf("Too-low port %sw as not validated correctly", portTooLow)
 	}
+
+	portTooHigh := "9001"
+	if isPortValid(portTooHigh) {
+		t.Errorf("Too-high port %s was not validated correctly", portTooHigh)
+
+	}
+}
+
+func TestHttpEncode(t *testing.T) {
+	//var expEncPw = "ZEHhWB65gUlzdVwtDQArEyx+KVLzp/aTaRaPlBzYRIFj6vjFdqEb0Q5B8zVKCZ0vKbZPZklJz0Fd7su2A+gf7Q=="
+
+	reqUrl := "http://localhost:8080"
+	reqResource := "/hash"
+	reqData := url.Values{}
+	reqData.Set("password", "angryMonkey")
+
+	u, _ := url.ParseRequestURI(reqUrl)
+	u.Path = reqResource
+	urlStr := u.String()
+
+	client := &http.Client{}
+	r, _ := http.NewRequest("POST", urlStr, strings.NewReader(reqData.Encode()))
+
+	resp, _ := client.Do(r)
+
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Unexpected status: %v", resp.Status)
+	}
+
+	//if resp != expEncPw {
+	//	t.Errorf("Received encoding did not match expected;\nExpected: '%s'\nReceived: '%s'\n", expEncPw, resp)
+	//}
 }
